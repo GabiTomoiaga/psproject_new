b85c0436636cecb6c96c3e8ff2afe44f80ed88f9
Gabi <gabi@gmail.com>
2025-05-30 19:15:54 +0300

    Added the perfume repository

--- PerfumeService/src/Domain/IPerfumeRepository.java
+++ PerfumeService/src/Domain/IPerfumeRepository.java
@@ -3,5 +3,10 @@ package Domain;
 import java.util.List;
 
 public interface IPerfumeRepository {
-
+    Boolean addPerfume(Perfume perfume);
+    Boolean deletePerfume(Perfume perfume);
+    Boolean updatePerfume(Perfume perfume);
+    Perfume searchPerfume(String name);
+    List<Perfume> listPerfumes();
+    List<Perfume> filterPerfumes(List<Perfume> perfumes, String brand, Boolean available, Double maxPrice, Integer storeId);
 }
